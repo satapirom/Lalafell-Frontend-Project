2a417b4816031f6e7d77fd1f042e68cdc439f1ee
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-09-23 14:39:21 +0700

    009- commit: added prodUrl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5000";
+const PROD_URL = "https://lalafell-backend-project-nddk.vercel.app";
+
+export { BASE_URL, PROD_URL };
